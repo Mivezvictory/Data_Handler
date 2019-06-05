a38f6c25974e004280d7fb1d0b36353116f064ca
DESKTOP-8VEJUR0\CEOS <nchrisr@myumanitoba.ca>
2019-06-05 15:20:34 -0500

    -> Updated the UI screens to allow for changing upon button click. -> Added an info.txt to describe the needs for the Screens to work.

--- UI/StartPage.py
+++ UI/StartPage.py
@@ -1,20 +1,32 @@
-import Tkinter as tk
-from Tkinter import *
-import DataEntry
+import tkinter as tk
 
 
-class StartPage(tk.Frame):
+class StartPage:
     def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        self.controller = controller
-        label = tk.Label(self, text="Welcome to the avocado clan lads", font=12)
-        label.pack(pady=10, padx=10)
+
+        # The parent will be the main frame of the DataEntry Object (DataEntry.my_containter_frame),
+        # which this will be placed into.
+        # The controller argument is the DataEntry object itself
+        self.parent = parent
+        self.my_controller = controller
+
+        # A frame is an invisible box that we put stuff into.
+        # Pack the frame up and place a label
+        self.my_frame = tk.Frame(self.parent, bg="pink")
+        self.my_frame.pack(fill=tk.BOTH, expand=True)
+        self.label = tk.Label(self.my_frame, text="Welcome to the avocado clan lads", font=12)
+        self.label.pack(pady=10, padx=10)
+
         init_x = 0.3
         init_y = 0.3
         button_width = 0.4
         button_height = 0.05
-        load_template = tk.Button(self, text="Load a template", bg="#EAECEE", fg="#34495E", font=10,
-                               command=lambda: controller.show_frame("DataApp"))
-        load_template.place(relx=init_x, rely=init_y, relwidth=button_width, relheight=button_height)
-        load_template.config(highlightbackground="black")
+
+        # Make the button in the middle, and assign the command argument to load a different page.
+        # my_controller.show_frame loads up the new frame.
+        self.load_template_button = tk.Button(self.my_frame, text="Load a template", bg="#EAECEE", fg="#34495E",
+                                              font=10, command=lambda: self.my_controller.show_frame("DataApp"))
+        self.load_template_button.place(relx=init_x, rely=init_y, relwidth=button_width, relheight=button_height)
+        self.load_template_button.config(highlightbackground="black")
+
         #load_template.pack()
